3236cec018d29739532032a13dd58cf14116238a
DOVENE AGOGUE <adovene@gmail.com>
2024-04-03 21:00:31 +0200

    add database impl

--- src/main/java/com/dev/App.java
+++ src/main/java/com/dev/App.java
@@ -1,7 +1,9 @@
 package com.dev;
 
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Hello world!
@@ -12,6 +14,16 @@ public class App {
     public static void main(String[] args) {
         
         System.out.println("Hello World!");
+        
+        try {
+            ClientRepository clientRepository = new ClientRepositoryImpl();
+            Client client = new Client(new Random().nextInt(100000),"test",new Date(), 2.0);
+            addClient(client, clientRepository);
+            displayAndGetClients(clientRepository);
+        } catch (ClassNotFoundException | SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 
     public static List<Client> displayAndGetClients(ClientRepository clientRepository) throws ClassNotFoundException, SQLException {
--- src/main/java/com/dev/ClientRepositoryImpl.java
+++ src/main/java/com/dev/ClientRepositoryImpl.java
@@ -1,15 +1,48 @@
 package com.dev;
 
+import java.sql.Connection;
 import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ClientRepositoryImpl implements ClientRepository{
 
     @Override
     public boolean addClient(Client client) throws ClassNotFoundException, SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addStudent'");
+        ClientDBConfig dbConfig = new ClientDBConfig();
+        Connection connection = dbConfig.getConnection();
+        String insertionQuery = "INSERT INTO client (id, name, contactDate, dailyBillAmount) VALUES (?, ?, ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(insertionQuery);
+        preparedStatement.setInt(1, client.getId());
+        preparedStatement.setString(2, client.getName());
+        preparedStatement.setDate(3, new java.sql.Date(client.getContactDate().getTime()));
+        preparedStatement.setDouble(4, client.getDailyBillAmount());
+        int rowsAffected = preparedStatement.executeUpdate();
+        return rowsAffected > 0;
+    }
+
+    @Override
+    public List<Client> getAllClients() throws ClassNotFoundException, SQLException {
+        ArrayList<Client> clients = new ArrayList<>();
+        ClientDBConfig dbConfig = new ClientDBConfig();
+        Connection connection = dbConfig.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * from client");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String name = resultSet.getString("name");
+            Date contactDate = resultSet.getDate("contactDate");
+            double dailyBillAmount = resultSet.getDouble("dailyBillAmount");
+            System.out.println("id: " + id);
+            clients.add(new Client(id, name, contactDate, dailyBillAmount));
+        }
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        return clients;
     }
 
     @Override
@@ -22,12 +55,6 @@ public class ClientRepositoryImpl implements ClientRepository{
     public boolean updateClient(int id) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'updateStudent'");
-    }
-
-    @Override
-    public List<Client> getAllClients() throws ClassNotFoundException, SQLException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAllClients'");
     }
 
     @Override
